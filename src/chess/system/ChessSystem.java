3e1952b98ac64c2b7ef80557ae3e0474abd9f23f
Leonardo Winch Dallanora <leonardo.dallanora@gmail.com>
2024-02-28 19:28:02 -0300

    Project created

---
+++ src/chess/system/ChessSystem.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package chess.system;
+
+
+public class ChessSystem {
+
+    
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+    }
+    
+}
