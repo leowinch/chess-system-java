aa61a9240b6c3a2274c5b2e28055bb456f8f03f6
Leonardo Winch Dallanora <leonardo.dallanora@gmail.com>
2024-02-29 16:12:08 -0300

    Placing pieces on theboard

---
+++ src/chess/pieces/Rook.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package chess.pieces;
+
+import boardgame.Board;
+import chess.ChessPiece;
+import chess.Color;
+
+
+public class Rook extends ChessPiece{
+    
+    public Rook(Board board, Color color) {
+        super(color, board);
+    }
+    
+    @Override
+    public String toString() {
+        return "R";
+    }
+}
