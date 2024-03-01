5b6a9352165fc248a7d47691e867e598c70f1256
Leonardo Winch Dallanora <leonardo.dallanora@gmail.com>
2024-03-01 17:23:35 -0300

    BoardException and defensive programming

---
+++ src/boardgame/BoardException.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package boardgame;
+
+
+public class BoardException extends RuntimeException{
+    
+    public BoardException(String msg) {
+        super(msg);
+    }
+}
