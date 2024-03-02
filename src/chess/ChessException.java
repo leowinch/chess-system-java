51bec52ef20ff22be4b6a906c59597b3e90de19c
Leonardo Winch Dallanora <leonardo.dallanora@gmail.com>
2024-03-01 22:33:25 -0300

    Moving Pieces

--- src/chess/ChessException.java
+++ src/chess/ChessException.java
@@ -4,8 +4,10 @@
  */
 package chess;
 
+import boardgame.BoardException;
 
-public class ChessException extends RuntimeException {
+
+public class ChessException extends BoardException {
     
     public ChessException(String msg) {
         super(msg);
